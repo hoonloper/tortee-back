001959903c25dfd9237e630d71d775085e274cd8
Yonghoon <yonghoonj98@gmail.com>
2022-12-10 22:28:11 +0900

    Fix: import 코드 및 주입 제거

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { POSTGRESQL_CONFIG } from './common/configs/postgresql.config';
 
 @Module({
@@ -10,7 +8,7 @@ import { POSTGRESQL_CONFIG } from './common/configs/postgresql.config';
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync(POSTGRESQL_CONFIG),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
