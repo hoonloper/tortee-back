7b19c38ea385c47c1ff7864d22da70f9246ce961
Yonghoon <yonghoonj98@gmail.com>
2022-12-10 22:16:19 +0900

    Set: PostgreSQL 연결 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { POSTGRESQL_CONFIG } from './common/configs/postgresql.config';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync(POSTGRESQL_CONFIG),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
