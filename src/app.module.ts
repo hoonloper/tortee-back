c4685cad05619a99e470eb254144fe3c18e6c8f8
Yonghoon <yonghoonj98@gmail.com>
2022-12-10 22:35:47 +0900

    Set: Winston 로거 설치 및 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,22 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { WinstonModule } from 'nest-winston';
 import { POSTGRESQL_CONFIG } from './common/configs/postgresql.config';
+import { WINSTON_CONFIG } from './common/configs/winston.config';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync(POSTGRESQL_CONFIG),
+    WinstonModule.forRootAsync(WINSTON_CONFIG),
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
